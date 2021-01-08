ee10b963c73a31f5cc82606ed2a242d0d2d6b2fe
Zohre Salimi <zohre.1987@gmail.com>
2021-01-08 11:28:55 +0100

    add favorites and watch later design

--- src/components/CardItem/index.js
+++ src/components/CardItem/index.js
@@ -1,13 +1,26 @@
 import React, { useState, useContext } from "react";
 
-import { Card, Image, Button, Dimmer } from "semantic-ui-react";
+import {
+  Card,
+  Image,
+  Segment,
+  Dimmer,
+  Button,
+  Label,
+  Icon,
+  Popup,
+} from "semantic-ui-react";
 
 import AppContext from "../../store/context";
+
+import "./style.css";
 
 import PlayButton from "../PlayButton";
 export default function CardItem({ item }) {
   const { state, dispatch } = useContext(AppContext);
   const [playVideo, setPlayVideo] = useState(false);
+  const [favoriteList, setFavoriteList] = useState(false);
+  const [watchLaterList, setWatchLaterList] = useState(false);
 
   const [active, setactive] = useState();
   const { apiConfig } = state;
@@ -15,6 +28,12 @@ export default function CardItem({ item }) {
   const content = (
     <div>
       <PlayButton setPlayVideo={setPlayVideo} />
+      <div className="tools-wrapper">
+        <Button.Group vertical basic size="small">
+          <Button icon="star" />
+          <Button icon="clock" />
+        </Button.Group>
+      </div>
     </div>
   );
   return (
@@ -31,7 +50,6 @@ export default function CardItem({ item }) {
           size="small"
           src={`${apiConfig.secure_base_url}/${apiConfig.poster_sizes[4]}/${item.poster_path}`}
         />
-
         <Card.Content>
           <Card.Header>{item.title}</Card.Header>
         </Card.Content>
